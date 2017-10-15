12a407a59e8a0f3dcfd9e451466dd7382cfb7dfe
Lauren Shin <laurenshin15@gmail.com>
2017-10-15 13:43:20 -0700

    minor changes

--- src/OverlordManager.cpp
+++ src/OverlordManager.cpp
@@ -4,19 +4,75 @@
 #include "CCBot.h"
 #include "Common.h"
 #include "sc2api/sc2_api.h"
+#include "sc2api/sc2_action.h"
 
-// Spreads Overlord out across the map after they spawn, makes them patrol
-// @moomoo	Overlord
-// @place	Coordinates of destination
-// @bot		Bot thing, necessary thing
-// maybe pass in vector of points to scout if possible
-// std::vector<sc2::Point2D> & blah ?
+// Moves Overlord to point specified
+// @moomoo		Overlord
+// @destination	Coordinates of destination
+// @bot			Bot thing, necessary thing
 void OverlordManager::OverlordMove(const sc2::Unit & moomoo, const sc2::Point2D & destination, CCBot & bot)
 {
 	// how to access map information? 
 
 	// move overlord to given position
 	bot.Actions()->UnitCommand(moomoo, sc2::ABILITY_ID::MOVE, destination);
+}
+
+// Moves Overlord to points specified
+// @moomoo			Overlord
+// @destinations	Coordinates of destination
+// @bot				Bot thing, necessary thing
+// how to queue up unit move commands?
+void OverlordManager::OverlordMove(const UnitTag & moomoo, const std::vector<sc2::Point2D> & destinations, CCBot & bot)
+{
+	// CURRENT STRUCTURE DOES NOT WORK
+	// WILL RESET INDEX TO 0 EVERY TIME IT IT CALLED, AKA EACH FRAME
+	int index = 0;
+
+	// queue up points from destinations for overlord passed
+
+	while (index < destinations.size())
+	{
+		if (!(bot.GetUnit(moomoo)->pos.x == destinations[index].x && bot.GetUnit(moomoo)->pos.y == destinations[index].y))
+		{
+			bot.Actions()->UnitCommand(moomoo, sc2::ABILITY_ID::MOVE, destinations[index]);
+			return;
+		}
+		else
+		{
+			index++;
+		}
+	}
+
+
+	//
+	//// first in int vector is unit tag
+	//std::vector<std::vector> temp = { int(moomoo) };
+
+	//// second and on is another vector with coordinates of point2d to move to
+	//// separated by a negative one in first vector
+	//int index = 1;
+
+	//for (auto & point : destinations)
+	//{
+	//	temp.insert(temp.end(), { int(point.x), int(point.y) });
+	//	temp.insert(temp.end(), -1);
+	//}
+
+	//if (bot.GetUnit(temp[0])->pos.x == temp[index][0] && bot.GetUnit(temp[0])->pos.y == temp[index][1])
+	//{
+	//	bot.Actions->UnitCommand(moomoo, sc2::ABILITY_ID::MOVE, temp);
+	//}
+	//else
+	//{
+	//	index++;
+	//	if (index > destinations.end())
+	//	{
+	//		return;
+	//	}
+	//}
+
+	
 }
 
 // makes and enforces overlord personal space bubble
@@ -68,7 +124,14 @@ void OverlordManager::Execute(CCBot & bot)
 			}
 
 			// call function, move overlord to enemyBaseLocation
-			OverlordManager::OverlordMove(unit, enemyBaseLocation->getPosition(), bot);
+			//OverlordManager::OverlordMove(unit, enemyBaseLocation->getPosition(), bot);
+			const sc2::Point2D & point = sc2::Point2D(50, 50);
+			const sc2::Point2D & point2 = sc2::Point2D(enemyBaseLocation->getPosition().x, enemyBaseLocation->getPosition().y);
+
+			std::vector<sc2::Point2D> places = { point, point2 };
+
+
+			OverlordManager::OverlordMove(unit, places, bot);
 		}
 	}
 }
